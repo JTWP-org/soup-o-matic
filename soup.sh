0bc5b91832e1ced4f53ac325608c8211fbc6b9f2
Salted Cracker Jack <87721660+oneSALTycracker@users.noreply.github.com>
2022-05-09 14:49:02 -0400

    Update soup.sh

--- soup.sh
+++ soup.sh
@@ -3,37 +3,18 @@
 #config here with these variables
 
 ###############################################################
-WEBHOOKURL='placeWebhookUrlHere'
+WEBHOOKURL='https://canary.discord.com/api/webhooks/967407890803597332/5LtxTlnRpfjyc25KJKEt2XfKw2HsSz3WAx63t-lzGaHcwGXJzGUevZqEAYSqqw6y8fRO'
 #Webhooks urls can be got from discord server
 
-soup=(
-
-"Toasted Orzo Chicken Soup https://www.rachaelraymag.com/recipe/toasted-orzo-chicken-soup"
-"Mexican Chicken Lime Soup https://www.rachaelraymag.com/recipe/mexican-chicken-lime-soup"
-
-)
-
-##############################################################
-#randomizer 
-
-#find total # of soups 
-Na=$((${#soup[@]}-1))
-
-#picks random string
-randomnum=$(jot -r 1 0 $Na)
-
-#sets random string as var $choice
-choice="${a[$randomnum]}"
-##############################################################
-
-
-
+FILELOC='/home/steam/soup'
+#where is discord.sh located at
+# I.E.  '/home/steam/soup'
 
 #################
-#webhook section below remove everything after this to not use webhook or comment out 
+#webhook section
+
 
 $FILELOC/discord.sh \
   --webhook-url="$WEBHOOKURL" \
-  --username "NotificationBot" \
-  --avatar "https://i.imgur.com/12jyR5Q.png" \
-  --text "today's JTWP soup of the day is `$choice`"
+  --username "Noino" \
+  --text `shuf -n 1 /home/steam/soup/soupbox` \
